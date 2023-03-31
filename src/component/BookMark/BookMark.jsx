f74297cff70c208f1b6d031aa7918018d9188378
Nahid-subi <scientisthassannahid@gmail.com>
2023-03-31 10:16:06 +0600

    Bookmark section design done

---
+++ src/component/BookMark/BookMark.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './BookMark.css'
+const BookMark = () => {
+    return (
+        <div>
+            <h3 className='time-read'>Spent time on read : </h3>
+            <div className='book-mark-add'>
+                <h3 className='mark-count'>Bookmarked Blogs : </h3>
+                <h4 className='mark-title'>Master Microsoft Power Platform and Become an In-Demand!</h4>
+            </div>
+        </div>
+    );
+};
+
+export default BookMark;
