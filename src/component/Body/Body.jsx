e86755859e22b14949fd1b29aae942f2ecc1593b
Nahid-subi <scientisthassannahid@gmail.com>
2023-03-31 10:13:33 +0600

    card section done and Bookmark file create

--- src/component/Body/Body.jsx
+++ src/component/Body/Body.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Card from '../Card/Card';
 import './Body.css'
+import BookMark from '../BookMark/BookMark';
 const Body = () => {
     return (
         <div className='body'>
             <div className='card'>
                 <Card></Card>
             </div>
-            <div className='side-bar'>sa</div>
+            <div className='side-bar'>
+                <BookMark></BookMark>
+            </div>
         </div>
     );
 };
--- src/component/ShowCard/ShowCard.jsx
+++ src/component/ShowCard/ShowCard.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './ShowCard.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 const ShowCard = (props) => {
-    const {authorName} = props.card;
+    const { authorName, authorImg, blogTitle, blogImg, readTime } = props.card;
     return (
-        <div>
-            <h1>dsfs {authorName}</h1>
+        <div className='card-style'>
+            <img className='img' src={blogImg} alt="" />
+            <div className='author'>
+                <div className='author-info'>
+                    <img src={authorImg} alt="" />
+                    <div>
+                        <h5>{authorName}</h5>
+                        <p>Mar 14 (4 Days ago)</p>
+                    </div>
+                </div>
+                <p className='book-mark'>{readTime} min read <FontAwesomeIcon icon={faBookmark} /></p>
+
+            </div>
+            <h1>{blogTitle}</h1>
+            <p className='p'>Mark as read</p>
+            <div className='hr'></div>
         </div>
     );
 };
