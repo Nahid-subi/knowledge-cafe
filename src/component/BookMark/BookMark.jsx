87411528dc40d1e3966d0f1c5b8bc3c5534f351a
Nahid-subi <scientisthassannahid@gmail.com>
2023-04-01 12:13:57 +0600

    bookmark complate

--- src/component/BookMark/BookMark.jsx
+++ src/component/BookMark/BookMark.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import './BookMark.css'
+import React, { useEffect, useState } from 'react';
+import './BookMark.css';
+
 const BookMark = () => {
-    return (
-        <div>
-            <h3 className='time-read'>Spent time on read : </h3>
-            <div className='book-mark-add'>
-                <h3 className='mark-count'>Bookmarked Blogs : </h3>
-                <h4 className='mark-title'>Master Microsoft Power Platform and Become an In-Demand!</h4>
-            </div>
+  const [bookmarkedBlogs, setBookmarkedBlogs] = useState([]);
+  const [totalTime, setTotalTime] = useState(0);
+
+  
+
+  useEffect(() => {
+    const savedCards = JSON.parse(localStorage.getItem('savedCards')) || [];
+    const savedTitles = savedCards.map((card) => card.blogTitle);
+    setBookmarkedBlogs(savedTitles);
+
+  
+  }, [bookmarkedBlogs.length]);
+
+  const handleReload = () => {
+    location.reload();
+  }
+
+  return (
+    <div>
+      <h3 className='time-read'>Spent time on read : {totalTime} seconds</h3>
+      {bookmarkedBlogs.length > 0 ? (
+        <div className='book-mark-add'>
+          <h3 className='mark-count'>Bookmarked Blogs : </h3>
+          <ul>
+            {bookmarkedBlogs.map((title, index) => (
+              <li key={index} className='mark-title'>{title}</li>
+            ))}
+          </ul>
         </div>
-    );
+      ) : (
+        <p>No bookmarked blogs</p>
+      )}
+      <div className='btn-show'>
+        <button className='show-btn' onClick={handleReload}>Click Here to Show Bookmark</button>
+      </div>
+    </div>
+  );
 };
 
 export default BookMark;
